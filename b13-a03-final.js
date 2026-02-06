cad3c3a3d19475b7df240020a72038f558c52107
Muin Chowdhury <muin.bd@gmail.com>
2026-02-06 20:45:33 +0600

    Problem 3 solved

--- b13-a03-final.js
+++ b13-a03-final.js
@@ -17,46 +17,6 @@ function newPrice(currentPrice, discount) {
 
 //Problem-02: OTP Validation for Zapshift
 
-/* 
- Function Name Must be validOtp()
-
-Zapshift কোম্পানি তাদের প্রোডাক্ট ডেলিভারি করার সময় OTP পাঠায়। কিন্তু কিছু লোক Fraud OTP ব্যবহার করে প্রোডাক্ট রিসিভ করে ফেলে। তাই তোমার কাজ হলো একটি ফাংশন বানানো, যা OTP সঠিক কিনা চেক করবে। 
-ফাংশনটি একটি Boolean (true/false) রিটার্ন করবে। যদি ইনপুটটি সঠিক ফরম্যাটে না থাকে তবে Invalid  রিটার্ন করবে। 
-
-Input :  ইনপুট হবে একটি string.
-
-
-🚀 Output: 
-Boolean true বা false রিটার্ন করবে
-
-
-🚩 Challenge:
-যদি ইনপুটটি String না হয় (যেমন: Number, Array বা Object), তবে আউটপুট হিসেবে   Invalid রিটার্ন করতে হবে।
-
-  
-💡 Validation Rules: 
-১. OTP অবশ্যই ৮ অক্ষরের (length) হতে হবে। 
-২. OTP অবশ্যই "ph-" দিয়ে শুরু হতে হবে।
-
-
-Hints 💡: startsWith() মেথড ব্যবহার করতে পারো। 
-
-
-SAMPLE INPUT
-SAMPLE OUTPUT
-"ph-10985"
-true
-"ph-1234"
-false
-"abc-12345"
-false
-["ph-10985"]
-Invalid
-12345678
-Invalid
-
-*/
-
 function validOtp(otp) {
   if (typeof otp !== "string") {
     return "Invalid";
@@ -71,4 +31,20 @@ function validOtp(otp) {
   }
 }
 
-console.log(validOtp(12345678));
+//Problem-03: BCS Final Score Calculator
+
+function finalScore(omr) {
+  if (
+    typeof omr !== "object" ||
+    typeof omr.right !== "number" ||
+    typeof omr.wrong !== "number" ||
+    typeof omr.skip !== "number"
+  ) {
+    return "Invalid";
+  } else if (omr.right + omr.wrong + omr.skip !== 100) {
+    return "Invalid";
+  } else {
+    const score = omr.right * 1 - omr.wrong * 0.5;
+    return Math.round(score);
+  }
+}
